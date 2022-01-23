0794034e603c703bd5a64198ba7083b8a547ee86
maor omri <maor.hightech@gmail.com>
2022-01-23 20:24:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const headerLinks = document.getElementsByClassName(".general-links");
+
+headerLinks.addEventListener("mouseup", () => {
+  headerLinks.style.color = red;
+});
+
+headerLinks.addEventListener("mousedown", () => {
+  headerLinks.style.color = red;
+});
+
+const tryNow = (document.querySelector("#try").textContent = "new text");
